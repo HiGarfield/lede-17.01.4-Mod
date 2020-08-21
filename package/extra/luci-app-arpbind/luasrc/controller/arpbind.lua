901c1d5d7c8e76c256fa21cecd597dde43719bc0
HiGarfield <higarfield@126.com>
2020-08-21 14:48:51 +0800

    luci-app-arpbind: update code

--- package/extra/luci-app-arpbind/luasrc/controller/arpbind.lua
+++ package/extra/luci-app-arpbind/luasrc/controller/arpbind.lua
@@ -1,8 +1,3 @@
---[[
- 静态ARP绑定 Luci页面 Controller
- Copyright (C) 2015 GuoGuo <gch981213@gmail.com>
-]]--
-
 module("luci.controller.arpbind", package.seeall)
 
 function index()
--- package/extra/luci-app-arpbind/luasrc/model/cbi/arpbind.lua
+++ package/extra/luci-app-arpbind/luasrc/model/cbi/arpbind.lua
@@ -1,36 +1,37 @@
---[[
- 静态ARP绑定 Luci页面 CBI page
- Copyright (C) 2015 GuoGuo <gch981213@gmail.com>
-]]--
-
 local sys = require "luci.sys"
+local ipc = require "luci.ip"
 local ifaces = sys.net:devices()
 
-m = Map("arpbind", translate("IP/MAC Binding"),
-        translatef("ARP is used to convert a network address (e.g. an IPv4 address) to a physical address such as a MAC address.Here you can add some static ARP binding rules."))
+m = Map("arpbind", translate("IP/MAC Binding"), 
+		translatef("ARP is used to convert a network address (e.g. an IPv4 address) to a physical address such as a MAC address. Here you can add some static ARP binding rules."))
 
 s = m:section(TypedSection, "arpbind", translate("Rules"))
 s.template = "cbi/tblsection"
 s.anonymous = true
 s.addremove = true
 
-a = s:option(Value, "ipaddr", translate("IP Address"))
-a.datatype = "ipaddr"
-a.optional = false
+ip = s:option(Value, "ipaddr", translate("IPv4 Address"))
+ip.datatype = "ip4addr"
+ip.optional = false
 
-a = s:option(Value, "macaddr", translate("MAC Address"))
-a.datatype = "macaddr"
-a.optional = false
+mac = s:option(Value, "macaddr", translate("MAC Address"))
+mac.datatype = "list(macaddr)"
+mac.optional = false
+
+ipc.neighbors({ family = 4 }, function(n)
+	if n.mac and n.dest then
+		ip:value(n.dest:string())
+		mac:value(n.mac, "%s (%s)" %{ n.mac, n.dest:string() })
+	end
+end)
 
 a = s:option(ListValue, "ifname", translate("Interface"))
 for _, iface in ipairs(ifaces) do
-	if iface ~= "lo" then 
-		a:value(iface) 
+	if iface ~= "lo" then
+		a:value(iface)
 	end
 end
 a.default = "br-lan"
 a.rmempty = false
 
 return m
-
-
