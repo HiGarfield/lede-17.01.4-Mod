cbe7be510e6da2263b91f427bca4c2612f1a116d
HiGarfield <higarfield@126.com>
2020-08-28 00:15:13 +0800

    update clean_all.sh

--- clean_all.sh
+++ clean_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf bin build_dir/* tmp staging_dir out .config .config.old
-[ -L dl ] && rm -f dl
+rm -rf dl
 make -C scripts/config clean >/dev/null 2>&1
 make -C feeds/luci/modules/luci-base/src clean >/dev/null 2>&1
