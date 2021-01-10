6dda1a6b575ebc440ee1bc85b61444901eac5757
HiGarfield <higarfield@126.com>
2021-01-11 01:44:36 +0800

    base-files: make it possible to specify switch led mode
    The swconfig switch led driver has the ability to switch
    between a "link, rx and/or tx" mode. However, this feature
    was not implemented in uci, the led init script and
    config_generate.
    
    This patch adds a seventh parameter to the
    ucidef_set_led_switch() function. The accepted values for
    this parameter are: link, rx and tx.
    Any permutations of these three values are supported, as
    long as they are properly encased with quotes.
    If the parameter is not specified it will default to "all"
    (link rx tx).
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -445,12 +445,14 @@ ucidef_set_led_switch() {
 	local trigger="$4"
 	local port_mask="$5"
 	local speed_mask="$6"
+	local mode="$7"
 
 	json_select_object led
 
 	json_select_object "$1"
 	json_add_string name "$name"
 	json_add_string type switch
+	json_add_string mode "$mode"
 	json_add_string sysfs "$sysfs"
 	json_add_string trigger "$trigger"
 	json_add_string port_mask "$port_mask"
