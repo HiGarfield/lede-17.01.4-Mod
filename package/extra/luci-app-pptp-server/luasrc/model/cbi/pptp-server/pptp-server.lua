ea78aa5b46f8ae1a8e5da92199c7f7b4c845b4f5
HiGarfield <higarfield@126.com>
2021-01-17 22:17:40 +0800

    luci-app-pptp-server: fix typos

--- package/extra/luci-app-pptp-server/luasrc/model/cbi/pptp-server/pptp-server.lua
+++ package/extra/luci-app-pptp-server/luasrc/model/cbi/pptp-server/pptp-server.lua
@@ -4,7 +4,7 @@
 mp = Map("pptpd", "PPTP Server","")
 
 s = mp:section(NamedSection, "pptpd", "service", translate("PPTP Service"))
-s.anonymouse = true
+s.anonymous = true
 --s.addremove = true
 
 enabled = s:option(Flag, "enabled", translate("Enable"))
@@ -26,7 +26,7 @@ logging.default = 0
 logging.rmempty = false
 
 logins = mp:section(NamedSection, "login", "login", translate("PPTP Logins"))
-logins.anonymouse = true
+logins.anonymous = true
 
 username = logins:option(Value, "username", translate("User name"))
 username.datatype = "string"
