97b40aad8d0ddf11d589db681ab420f6fc617556
HiGarfield <higarfield@126.com>
2021-01-28 13:10:36 +0800

    luci-app-wizard: load existing network configuration

--- package/extra/luci-app-wizard/luasrc/model/cbi/wizard/wizard.lua
+++ package/extra/luci-app-wizard/luasrc/model/cbi/wizard/wizard.lua
@@ -1,22 +1,23 @@
-local nt = require "luci.sys".net
 local uci = require("luci.model.uci").cursor()
 
 local has_wifi = false
-uci:foreach("wireless", "wifi-device",
-		function(s)
-			has_wifi = true
-			return false
-		end)
+uci:foreach("wireless", "wifi-device", function(s)
+    has_wifi = true
+    return false
+end)
 
-local m = Map("wizard", luci.util.pcdata(translate("Inital Router Setup")), translate("If you are using this router for the first time, please configure it here."))
+local m = Map("wizard", luci.util.pcdata(translate("Inital Router Setup")),
+              translate("If you are using this router for the first time, please configure it here."))
 
 local s = m:section(TypedSection, "wizard", "")
 s.addremove = false
 s.anonymous = true
 
-s:tab("wansetup", translate("Wan Settings"), translate("Three different ways to access the Internet, please choose according to your own situation."))
+s:tab("wansetup", translate("Wan Settings"),
+    translate("Three different ways to access the Internet, please choose according to your own situation."))
 if has_wifi then
-	s:tab("wifisetup", translate("Wireless Settings"), translate("Set the router's wireless name and password. For more advanced settings, please go to the Network-Wireless page."))
+    s:tab("wifisetup", translate("Wireless Settings"), translate(
+        "Set the router's wireless name and password. For more advanced settings, please go to the Network-Wireless page."))
 end
 s:tab("lansetup", translate("Lan Settings"))
 
@@ -24,51 +25,98 @@ local e = s:taboption("wansetup", ListValue, "wan_proto", translate("Protocol"))
 e:value("dhcp", translate("DHCP client"))
 e:value("static", translate("Static address"))
 e:value("pppoe", translate("PPPoE"))
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "proto") or "dhcp"
+end
 
 e = s:taboption("wansetup", Value, "wan_pppoe_user", translate("PAP/CHAP username"))
-e:depends({wan_proto="pppoe"})
+e:depends({
+    wan_proto = "pppoe"
+})
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "username")
+end
 
 e = s:taboption("wansetup", Value, "wan_pppoe_pass", translate("PAP/CHAP password"))
-e:depends({wan_proto="pppoe"})
+e:depends({
+    wan_proto = "pppoe"
+})
 e.password = true
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "password")
+end
 
 e = s:taboption("wansetup", Value, "wan_ipaddr", translate("IPv4 address"))
-e:depends({wan_proto="static"})
+e:depends({
+    wan_proto = "static"
+})
 e.datatype = "ip4addr"
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "ipaddr")
+end
 
 e = s:taboption("wansetup", Value, "wan_netmask", translate("IPv4 netmask"))
-e:depends({wan_proto="static"})
+e:depends({
+    wan_proto = "static"
+})
 e.datatype = "ip4addr"
 e:value("255.255.255.0")
 e:value("255.255.0.0")
 e:value("255.0.0.0")
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "netmask")
+end
 
 e = s:taboption("wansetup", Value, "wan_gateway", translate("IPv4 gateway"))
-e:depends({wan_proto="static"})
+e:depends({
+    wan_proto = "static"
+})
 e.datatype = "ip4addr"
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "gateway")
+end
 
 e = s:taboption("wansetup", DynamicList, "wan_dns", translate("Use custom DNS servers"))
---e:depends({wan_proto="dhcp"})
-e:depends({wan_proto="static"})
+-- e:depends({wan_proto="dhcp"})
+e:depends({
+    wan_proto = "static"
+})
 e.datatype = "ip4addr"
 e.cast = "string"
+e.cfgvalue = function(self, section)
+    return uci:get("network", "wan", "dns")
+end
 
 if has_wifi then
-	e = s:taboption("wifisetup", Value, "wifi_ssid", translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
-	e.datatype = "maxlength(32)"
+    e = s:taboption("wifisetup", Value, "wifi_ssid",
+            translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
+    e.datatype = "maxlength(32)"
+    e.cfgvalue = function(self, section)
+        return uci:get("wireless", "@wifi-iface[0]", "ssid")
+    end
 
-	e = s:taboption("wifisetup", Value, "wifi_key", translate("Key"))
-	e.datatype = "wpakey"
-	e.password = true
-end --has_wifi
+    e = s:taboption("wifisetup", Value, "wifi_key", translate("Key"))
+    e.datatype = "wpakey"
+    e.password = true
+    e.cfgvalue = function(self, section)
+        return uci:get("wireless", "@wifi-iface[0]", "key")
+    end
+
+end -- has_wifi
 
 e = s:taboption("lansetup", Value, "lan_ipaddr", translate("IPv4 address"))
 e.datatype = "ip4addr"
+e.cfgvalue = function(self, section)
+    return uci:get("network", "lan", "ipaddr")
+end
 
 e = s:taboption("lansetup", Value, "lan_netmask", translate("IPv4 netmask"))
 e.datatype = "ip4addr"
 e:value("255.255.255.0")
 e:value("255.255.0.0")
 e:value("255.0.0.0")
+e.cfgvalue = function(self, section)
+    return uci:get("network", "lan", "netmask")
+end
 
 return m
