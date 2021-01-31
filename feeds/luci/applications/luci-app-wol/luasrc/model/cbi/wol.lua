765475332e6e6f20c86f7da4ca2c9614751804bd
HiGarfield <higarfield@126.com>
2021-02-01 05:22:07 +0800

    luci-app-wol: update code

--- feeds/luci/applications/luci-app-wol/luasrc/model/cbi/wol.lua
+++ feeds/luci/applications/luci-app-wol/luasrc/model/cbi/wol.lua
@@ -1,94 +1,82 @@
--- Copyright 2010 Jo-Philipp Wich <jow@openwrt.org>
--- Licensed to the public under the Apache License 2.0.
-
 local sys = require "luci.sys"
-local fs  = require "nixio.fs"
+local fs = require "nixio.fs"
+local uci = require("luci.model.uci").cursor()
 
-m = SimpleForm("wol", translate("Wake on LAN"),
-	translate("Wake on LAN is a mechanism to remotely boot computers in the local network."))
-
-m.submit = translate("Wake up host")
-m.reset  = false
-
+m = Map("wol", translate("Wake on LAN"),
+        translate("Wake on LAN is a mechanism to remotely boot computers in the local network."))
 
 local has_ewk = fs.access("/usr/bin/etherwake")
 local has_wol = fs.access("/usr/bin/wol")
 
-
-s = m:section(SimpleSection)
+s = m:section(TypedSection, "hosts", translate("Hosts"))
+s.template = "cbi/tblsection"
+s.anonymous = true
+s.addremove = true
 
 if has_ewk and has_wol then
-	bin = s:option(ListValue, "binary", translate("WoL program"),
-		translate("Sometimes only one of the two tools works. If one fails, try the other one"))
-
-	bin:value("/usr/bin/etherwake", "Etherwake")
-	bin:value("/usr/bin/wol", "WoL")
+    bin = s:option(ListValue, "binary", translate("WoL program"))
+    bin:value("/usr/bin/etherwake", "Etherwake")
+    bin:value("/usr/bin/wol", "WoL")
 end
 
 if has_ewk then
-	iface = s:option(ListValue, "iface", translate("Network interface to use"),
-		translate("Specifies the interface the WoL packet is sent on"))
+    iface = s:option(ListValue, "iface", translate("Interface"))
+    iface.default = "br-lan"
 
-	if has_wol then
-		iface:depends("binary", "/usr/bin/etherwake")
-	end
+    if has_wol then
+        iface:depends("binary", "/usr/bin/etherwake")
+    end
 
-	iface:value("", translate("Broadcast on all interfaces"))
+    iface:value("", translate("Broadcast on all interfaces"))
 
-	for _, e in ipairs(sys.net.devices()) do
-		if e ~= "lo" then iface:value(e) end
-	end
+    for _, e in ipairs(sys.net.devices()) do
+        if e ~= "lo" then
+            iface:value(e)
+        end
+    end
 end
 
-
-host = s:option(Value, "mac", translate("Host to wake up"),
-	translate("Choose the host to wake up or enter a custom MAC address to use"))
+hostname = s:option(Value, "hostname", translate("Hostname"))
+macaddr = s:option(Value, "macaddr", translate("MAC address"))
 
 sys.net.mac_hints(function(mac, name)
-	host:value(mac, "%s (%s)" %{ mac, name })
+    macaddr:value(mac, "%s (%s)" % {mac, name})
+    hostname:value(name)
 end)
 
+btn = s:option(Button, "wake", "")
+btn.inputtitle = translate("Wake up host")
+btn.inputstyle = "apply"
+function btn.write(self, section)
+    local host = uci:get("wol", section, "macaddr")
+    if host and #host > 0 and host:match("^[a-fA-F0-9:]+$") then
+        local cmd
+        local util = uci:get("wol", section, "binary") or (has_ewk and "/usr/bin/etherwake" or "/usr/bin/wol")
+        if util == "/usr/bin/etherwake" then
+            local iface = uci:get("wol", section, "iface")
+            cmd = "%s -D%s %q" % {util, (iface ~= "" and " -i %q" % iface or ""), host}
+        else
+            cmd = "%s -v %q" % {util, host}
+        end
 
-function host.write(self, s, val)
-	local host = luci.http.formvalue("cbid.wol.1.mac")
-	if host and #host > 0 and host:match("^[a-fA-F0-9:]+$") then
-		local cmd
-		local util = luci.http.formvalue("cbid.wol.1.binary") or (
-			has_ewk and "/usr/bin/etherwake" or "/usr/bin/wol"
-		)
-
-		if util == "/usr/bin/etherwake" then
-			local iface = luci.http.formvalue("cbid.wol.1.iface")
-			cmd = "%s -D%s %q" %{
-				util, (iface ~= "" and " -i %q" % iface or ""), host
-			}
-		else
-			cmd = "%s -v %q" %{ util, host }
-		end
-
-		local msg = "<p><strong>%s</strong><br /><br /><code>%s<br /><br />" %{
-			translate("Starting WoL utility:"), cmd
-		}
-
-		local p = io.popen(cmd .. " 2>&1")
-		if p then
-			while true do
-				local l = p:read("*l")
-				if l then
-					if #l > 100 then l = l:sub(1, 100) .. "..." end
-					msg = msg .. l .. "<br />"
-				else
-					break
-				end
-			end
-			p:close()
-		end
-
-		msg = msg .. "</code></p>"
-
-		m.message = msg
-	end
+        local msg = "%s %s." % {translate("Starting WoL utility:"), cmd}
+        local p = io.popen(cmd .. " 2>&1")
+        if p then
+            while true do
+                local l = p:read("*l")
+                if l then
+                    if #l > 100 then
+                        l = l:sub(1, 100) .. "..."
+                    end
+                    msg = msg .. " " .. l
+                else
+                    break
+                end
+            end
+            p:close()
+        end
+        m.message = msg
+    end
 end
-
 
 return m
