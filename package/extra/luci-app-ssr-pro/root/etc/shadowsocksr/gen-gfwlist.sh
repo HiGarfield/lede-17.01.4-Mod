130881f95ae86da5c0ecf4561e3083a8cf75514e
HiGarfield <higarfield@126.com>
2021-02-07 23:49:39 +0800

    luci-app-ssr-pro: update up-gfwlist.sh

--- package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/gen-gfwlist.sh
+++ package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/gen-gfwlist.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-generate_china_banned()
-{
+generate_china_banned() {
 	local GFWLIST_URL
 	GFWLIST_URL=$(uci -q get ssrpro.@ssrpro[0].gfwlist_url)
 	[ -z "$GFWLIST_URL" ] && GFWLIST_URL="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
@@ -19,27 +18,19 @@ generate_china_banned()
 		exit 1
 	fi
 
-	if ( ! cmp -s /tmp/gfwlist.b64 /tmp/gfwlist2.b64 ); then
+	if (! cmp -s /tmp/gfwlist.b64 /tmp/gfwlist2.b64); then
 		rm -f /tmp/gfwlist.b64 >&2
 		rm -f /tmp/gfwlist2.b64 >&2
 		exit 1
 	fi
 	rm -f /tmp/gfwlist2.b64 >&2
 
-	base64 -d /tmp/gfwlist.b64 | sort -u |
+	base64 -d /tmp/gfwlist.b64 |
 		sed 's#!.\+##; s#|##g; s#@##g; s#https\?:\/\/##;' |
 		sed '/\*/d; /apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /byr\.cn/d; /jlike\.com/d; /weibo\.com/d; /zhongsou\.com/d; /youdao\.com/d; /sogou\.com/d; /so\.com/d; /soso\.com/d; /aliyun\.com/d; /taobao\.com/d; /jd\.com/d; /qq\.com/d' |
 		sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' |
-		grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' | sort -u |
-		awk '
-BEGIN { prev = "________"; }  {
-	cur = $0;
-	if (index(cur, prev) == 1 && substr(cur, 1 + length(prev) ,1) == ".") {
-	} else {
-		print cur;
-		prev = cur;
-	}
-}' | sort -u	
+		grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' |
+		awk 'BEGIN { prev = "________"; } { cur = $0; if (index(cur, prev) != 1 || substr(cur, 1 + length(prev), 1) != ".") { print cur; prev = cur; } }' | sort -u
 	rm -f /tmp/gfwlist.b64 >&2
 }
 
--- package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/up-gfwlist.sh
+++ package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/up-gfwlist.sh
@@ -3,22 +3,24 @@
 mkdir -p /var/lock
 lock -n /var/lock/up-gfwlist.lck
 
-/etc/shadowsocksr/gen-gfwlist.sh > /tmp/ol-gfw.txt
+/etc/shadowsocksr/gen-gfwlist.sh >/tmp/ol-gfw.txt
 
-if [ -s "/tmp/ol-gfw.txt" ];then
-	sort -u /etc/shadowsocksr/base-gfwlist.txt /tmp/ol-gfw.txt > /tmp/china-banned
-	if ( ! cmp -s /tmp/china-banned /etc/gfwlist/china-banned );then
-		if [ -s "/tmp/china-banned" ];then
-			mv /tmp/china-banned /etc/gfwlist/china-banned
-			echo "Update GFW-List Done!"
-		fi
-	else
-		echo "GFW-List No Change!"
+if [ -s "/tmp/ol-gfw.txt" ]; then
+	sort -u /etc/shadowsocksr/base-gfwlist.txt /tmp/ol-gfw.txt >/tmp/china-banned
+else
+	sort -u /etc/shadowsocksr/base-gfwlist.txt /etc/gfwlist/china-banned >/tmp/china-banned
+fi
+
+if (! cmp -s /tmp/china-banned /etc/gfwlist/china-banned); then
+	if [ -s "/tmp/china-banned" ]; then
+		mv -f /tmp/china-banned /etc/gfwlist/china-banned
+		echo "Update GFW-List Done!"
 	fi
+else
+	echo "GFW-List No Change!"
 fi
 
 rm -f /tmp/ol-gfw.txt
-
 /etc/init.d/ssrpro restart
 
 lock -u /var/lock/up-gfwlist.lck
