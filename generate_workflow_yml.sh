c848dbd2948a35429ae6114353926a0bc871e4bf
HiGarfield <higarfield@126.com>
2021-02-24 23:34:30 +0800

    update workflow files

--- generate_workflow_yml.sh
+++ generate_workflow_yml.sh
@@ -2,15 +2,23 @@
 
 generate_device_yml() {
 	local device_name="$1"
-	sed "s/template_device_please_replace/$device_name/" ".github/build-lede-template.yml" >".github/workflows/build-lede-${device_name}.yml"
+	local total_keep_num="$2"
+	sed -e "s/template_device_please_replace/$device_name/" \
+		-e "s/template_total_keep_number_please_replace/$total_keep_num/" \
+		".github/build-lede-template.yml" \
+		>".github/workflows/build-lede-${device_name}.yml"
 }
 
 mkdir -p ".github/workflows/"
-rm -rf .github/workflows/*
+rm -rf ".github/workflows/*"
+
+total_keep_num=$(($(find conf/.config.* -type f | wc -l) + 1))
 
 for file in conf/.config.*; do
 	dev_name="$(basename "$file" | sed 's/\.config\.//')"
-	generate_device_yml "$dev_name"
+	generate_device_yml "$dev_name" "$total_keep_num"
 done
 
-cp -f .github/lede-dl.yml .github/workflows/
+sed "s/template_total_keep_number_please_replace/$total_keep_num/" \
+	".github/lede-dl.yml" \
+	>".github/workflows/lede-dl.yml"
