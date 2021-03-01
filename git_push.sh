af8217069e94b0e00e521a7b1f49b7edd20a28bd
HiGarfield <higarfield@126.com>
2021-03-02 04:01:36 +0800

    git_push.sh: delete tags

--- git_push.sh
+++ git_push.sh
@@ -4,10 +4,11 @@
 	./clean_all.sh &&
 	./generate_workflow_yml.sh &&
 	./new_version.sh &&
-	ver_num=$(cat version)
+	ver_num=$(cat version) &&
 	git add . &&
 	git commit -m "$ver_num" &&
-	git clean -xfd  >/dev/null 2>&1
+	git clean -xfd  >/dev/null 2>&1 &&
+	git tag -d $(git tag -l) &&
 	git push --all origin --force &&
 	./make_tar.sh
 } || exit 1
