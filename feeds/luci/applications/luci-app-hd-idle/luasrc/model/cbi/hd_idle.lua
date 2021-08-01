0fae96832c9fc26b23fb308ffffcb453930850bf
HiGarfield <HiGarfield@126.com>
2021-08-02 01:42:07 +0800

    luci-app-hd-idle: add button "Spin down immediately"

--- feeds/luci/applications/luci-app-hd-idle/luasrc/model/cbi/hd_idle.lua
+++ feeds/luci/applications/luci-app-hd-idle/luasrc/model/cbi/hd_idle.lua
@@ -1,31 +1,54 @@
 -- Copyright 2008 Yanira <forum-2008@email.de>
 -- Licensed to the public under the Apache License 2.0.
-
 require("nixio.fs")
 
 m = Map("hd-idle", translate("HDD Idle"),
-	translate("HDD Idle is a utility program for spinning-down external " ..
-		"disks after a period of idle time."))
+    translate("HDD Idle is a utility program for spinning-down external " .. "disks after a period of idle time."))
 
 s = m:section(TypedSection, "hd-idle", translate("Settings"))
 s.anonymous = true
 s.addremove = true
 s.template = "cbi/tblsection"
+s.optional = false
 
-s:option(Flag, "enabled", translate("Enable"))
+enabled = s:option(Flag, "enabled", translate("Enable"))
+enabled.default = 1
+enabled.optional = false
 
 disk = s:option(Value, "disk", translate("Disk"))
-disk.rmempty = true
+disk.optional = false
 for dev in nixio.fs.glob("/dev/[sh]d[a-z]") do
-	disk:value(nixio.fs.basename(dev))
+    if nixio.fs.stat(dev, "type") == "blk" then
+        disk:value(nixio.fs.basename(dev))
+    end
 end
 
-s:option(Value, "idle_time_interval", translate("Idle time")).default = 10
-s.rmempty = true
+idle_time_interval = s:option(Value, "idle_time_interval", translate("Idle time"))
+idle_time_interval.default = 10
+idle_time_interval.optional = false
+idle_time_interval.maxlength = 6
+idle_time_interval.datatype = 'uinteger'
+
 unit = s:option(ListValue, "idle_time_unit", translate("Idle time unit"))
 unit.default = "minutes"
 unit:value("minutes", translate("min"))
 unit:value("hours", translate("h"))
-unit.rmempty = true
+unit.optional = false
+
+spindown = s:option(Button, "spin_down_immediately", translate("Spin down immediately"))
+spindown.render = function(self, section, scope)
+    if nixio.fs.stat("/dev/%s" % self.map:get(section, "disk"), "type") == "blk" then
+        self.inputstyle = "apply"
+    else
+        self.inputstyle = nil
+    end
+    Button.render(self, section, scope)
+end
+spindown.write = function(self, section)
+    local disk_dev = self.map:get(section, "disk")
+    if nixio.fs.stat("/dev/%s" % disk_dev, "type") == "blk" then
+        luci.sys.call("hd-idle -t '%s'" % disk_dev)
+    end
+end
 
 return m
