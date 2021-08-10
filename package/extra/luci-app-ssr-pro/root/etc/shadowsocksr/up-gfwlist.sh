e60b8753412b4c74496e0b6e169aa274ff37aeef
HiGarfield <HiGarfield@126.com>
2021-08-11 02:09:54 +0800

    luci-app-ssr-pro: improve script up-gfwlist.sh

--- package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/up-gfwlist.sh
+++ package/extra/luci-app-ssr-pro/root/etc/shadowsocksr/up-gfwlist.sh
@@ -11,9 +11,14 @@ else
 	sort -u /etc/shadowsocksr/base-gfwlist.txt /etc/gfwlist/china-banned >/tmp/china-banned
 fi
 
+sed -i '/^[[:space:]]*$/d' /tmp/china-banned
+
+need_restart=0
+
 if (! cmp -s /tmp/china-banned /etc/gfwlist/china-banned); then
 	if [ -s "/tmp/china-banned" ]; then
 		mv -f /tmp/china-banned /etc/gfwlist/china-banned
+		need_restart=1
 		echo "Update GFW-List Done!"
 	fi
 else
@@ -21,6 +26,6 @@ else
 fi
 
 rm -f /tmp/ol-gfw.txt
-/etc/init.d/ssrpro restart
+[ "$need_restart" -eq 1 ] && /etc/init.d/ssrpro restart
 
 lock -u /var/lock/up-gfwlist.lck
