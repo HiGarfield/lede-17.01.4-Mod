4b8519edcdd9e3aece21ddfe6d7ab4f9b56d2ea3
HiGarfield <HiGarfield@126.com>
2021-12-12 02:10:09 +0800

    build luci-app-easymesh by default

--- include/target.mk
+++ include/target.mk
@@ -30,7 +30,7 @@ luci-app-arpbind luci-app-eqos \
 luci-app-openvpn-server luci-app-pptp-server \
 luci-app-accesscontrol luci-app-cpulimit \
 luci-app-wifischedule luci-app-uhttpd xl2tpd \
-luci-app-syncdial
+luci-app-syncdial luci-app-easymesh
 
 ifneq ($(_CONF_ROUTER_WITHOUT_USB_PORT_),1)
 DEFAULT_PACKAGES += block-mount automount fdisk lsblk \
