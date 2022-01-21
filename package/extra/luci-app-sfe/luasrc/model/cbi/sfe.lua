ebdf480cced12532c30037ceb33cfc92e2eb5edc
HiGarfield <HiGarfield@126.com>
2022-01-22 06:08:15 +0800

    luci-app-sfe: improve code

--- package/extra/luci-app-sfe/luasrc/model/cbi/sfe.lua
+++ package/extra/luci-app-sfe/luasrc/model/cbi/sfe.lua
@@ -1,7 +1,7 @@
-local m, s, o
+local m, s, enabled, bridge, ipv6, offload_at_pkts
 local SYS = require "luci.sys"
 
-if SYS.call("lsmod | grep -q fast_classifier") == 0 then
+if SYS.call("lsmod | grep -qw \'^fast_classifier\'") == 0 then
     Status = translate("<strong><font color=\"green\">Shortcut forwarding engine is running</font></strong>")
 else
     Status = translate("<strong><font color=\"red\">Shortcut forwarding engine is not running</font></strong>")
@@ -16,25 +16,22 @@ s.addremove = false
 s.anonymous = true
 s.description = string.format("%s<br /><br />", Status)
 
-o = s:option(Flag, "enabled", translate("Enable"))
-o.default = 0
-o.rmempty = false
+enabled = s:option(Flag, "enabled", translate("Enable"))
+enabled.rmempty = false
 
-o = s:option(Flag, "bridge", translate("Bridge acceleration"))
-o.default = 0
-o.rmempty = false
-o.description = translate("Enable bridge acceleration")
+bridge = s:option(Flag, "bridge", translate("Bridge acceleration"))
+bridge.description = translate("Enable bridge acceleration")
+bridge:depends("enabled", "1")
 
-o = s:option(Flag, "ipv6", translate("IPv6 acceleration"))
-o.default = 0
-o.rmempty = false
-o.description = translate("Enable IPv6 acceleration")
+ipv6 = s:option(Flag, "ipv6", translate("IPv6 acceleration"))
+ipv6.description = translate("Enable IPv6 acceleration")
+ipv6:depends("enabled", "1")
 
-o = s:option(Value, "offload_at_pkts", translate("Accelerate at packets"))
-o.description = translate("Start acceleration after how many packets, defaultly 128, at least 4")
-o.datatype = "and(uinteger,min(4))"
-o.rmempty = true
-o.placeholder = 128
+offload_at_pkts = s:option(Value, "offload_at_pkts", translate("Accelerate at packets"))
+offload_at_pkts.description = translate("Start acceleration after how many packets, defaultly 128, at least 4")
+offload_at_pkts.datatype = "and(uinteger,min(4))"
+offload_at_pkts.placeholder = 128
+offload_at_pkts:depends("enabled", "1")
 
 function m.on_after_commit(self)
     os.execute("/etc/init.d/sfe restart >/dev/null 2>&1 &")
