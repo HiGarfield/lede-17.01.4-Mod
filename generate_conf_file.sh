e09a596a7a845c40d62e55184dfb6b3cebd341b7
HiGarfield <HiGarfield@126.com>
2022-08-03 07:06:24 +0800

    generate_conf_file.sh: add yyets-le2

--- generate_conf_file.sh
+++ generate_conf_file.sh
@@ -131,6 +131,7 @@ devices=(
 	"ramips_mt7621_DEVICE_ghl-r-001-f"
 	"ramips_mt7621_DEVICE_hw24g"
 	"ramips_mt7621_DEVICE_link7"
+	"ramips_mt7621_DEVICE_yyets-le2"
 )
 process_devices $devices "1004kc"
 
