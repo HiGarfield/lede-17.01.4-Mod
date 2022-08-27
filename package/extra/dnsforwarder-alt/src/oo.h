c337cbc2556a7fb57afa557a5cda575003c2fdab
HiGarfield <HiGarfield@126.com>
2022-08-27 16:50:33 +0800

    dnsforwarder-alt: switch to lifenjoiner's repo

--- package/extra/dnsforwarder-alt/src/oo.h
+++
@@ -1,9 +0,0 @@
-#ifndef OO_H_INCLUDED
-#define OO_H_INCLUDED
-
-#define PRIMEMB
-#define PUBMEMB
-#define PRIFUNC static
-#define PUBFUNC static
-
-#endif // OO_H_INCLUDED
--- package/extra/dnsforwarder-alt/src/test/testutils.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTUTILS_H_INCLUDED
-#define TESTUTILS_H_INCLUDED
-
-char *RandomString(char *out, int len);
-
-char *RandomAlpha(char *out, int len);
-
-#endif // TESTUTILS_H_INCLUDED
