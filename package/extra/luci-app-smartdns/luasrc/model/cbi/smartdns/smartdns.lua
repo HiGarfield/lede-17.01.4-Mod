e307633632ac4ba62ec734c61822689eea277383
HiGarfield <HiGarfield@126.com>
2022-09-03 01:18:33 +0800

    luci-app-smartdns: update

--- package/extra/luci-app-smartdns/luasrc/model/cbi/smartdns/smartdns.lua
+++ package/extra/luci-app-smartdns/luasrc/model/cbi/smartdns/smartdns.lua
@@ -106,7 +106,7 @@ o = s:taboption("settings", Value, "rr_ttl_max", translate("Domain TTL Max"), tr
 o.rempty      = true
 
 ---- rr-ttl-reply-max
-o = s:taboption("settings", Value, "rr_ttl_reply_max", translate("Domain TTL Max"), translate("Maximum Reply TTL for all domain result."))
+o = s:taboption("settings", Value, "rr_ttl_reply_max", translate("Reply Domain TTL Max"), translate("Reply maximum TTL for all domain result."))
 o.rempty      = true
 
 ---- second dns server
