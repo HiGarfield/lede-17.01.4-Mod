0f9b98005beaab7b23679e6c4ce99bd6cbeed348
HiGarfield <HiGarfield@126.com>
2023-01-30 02:09:10 +0800

    shellsync: refine code

--- package/network/services/shellsync/src/shellsync.c
+++ package/network/services/shellsync/src/shellsync.c
@@ -1,22 +1,21 @@
 /*************************************************************************
-	> File Name: shellsync.c
-	> Author: GuoGuo
-	> Mail: gch981213@gmail.com
-	> Created Time: 2014年11月06日 星期四 19时15分30秒
+    > File Name: shellsync.c
+    > Author: GuoGuo
+    > Mail: gch981213@gmail.com
+    > Created Time: 2014年11月06日 星期四 19时15分30秒
  ************************************************************************/
+
+#include <time.h>
 #include <stdio.h>
 #include <semaphore.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <time.h>
 #include <errno.h>
 
-#define SEM_BLOCK_NAME  "SYNCSHELL_block"
-#define SEM_COUNT_NAME  "SYNCSHELL_count"
+#define SEM_BLOCK_NAME "SYNCSHELL_block"
+#define SEM_COUNT_NAME "SYNCSHELL_count"
 
-int wait_timeout;
-
-int sync_wait(int nproc)
+static int sync_wait(int nproc, int wait_timeout)
 {
     int flags;
     int value;
@@ -24,13 +23,15 @@ int sync_wait(int nproc)
     sem_t *count;
     struct timespec ts;
 
-    if (nproc <= 1) {
-        printf("sync_wait: number of processes should be larger than 1\n");
+    if (nproc <= 1)
+    {
+        printf("sync_wait: number of processes should be larger than 1.\n");
         return -1;
     }
 
-    if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
-        printf("clock_gettime error\n");
+    if (clock_gettime(CLOCK_REALTIME, &ts) == -1)
+    {
+        printf("clock_gettime error.\n");
         return -1;
     }
     ts.tv_sec += wait_timeout;
@@ -38,35 +39,48 @@ int sync_wait(int nproc)
     flags = O_RDWR | O_CREAT;
     block = sem_open(SEM_BLOCK_NAME, flags, 0644, 0);
     count = sem_open(SEM_COUNT_NAME, flags, 0644, 0);
-    if (block == SEM_FAILED || count == SEM_FAILED) {
-        printf("sync_wait: sem_open failed\n");
+    if (block == SEM_FAILED || count == SEM_FAILED)
+    {
+        printf("sync_wait: sem_open failed.\n");
         return -1;
     }
 
-    if (sem_post(count) < 0) {
-        printf("sync_wait: sem_post failed\n");
+    if (sem_post(count) < 0)
+    {
+        printf("sync_wait: sem_post failed.\n");
         return -1;
     }
-    if (sem_getvalue(count, &value) < 0) {
-        printf("sync_wait: sem_getvalue failed\n");
+    if (sem_getvalue(count, &value) < 0)
+    {
+        printf("sync_wait: sem_getvalue failed.\n");
         return -1;
     }
-    printf("%d processes have arrived, waiting for the left %d\n", value, nproc-value);
+    printf("%d processes have arrived, waiting for the left %d.\n",
+           value, nproc - value);
 
-    if (value >= nproc) {
-        while (nproc-1 > 0) {
-            if (sem_post(block) < 0) {
-                printf("sync_wait: sem_post failed\n");
+    if (value >= nproc)
+    {
+        while (nproc - 1 > 0)
+        {
+            if (sem_post(block) < 0)
+            {
+                printf("sync_wait: sem_post failed.\n");
                 return -1;
             }
             nproc--;
         }
-    } else {
-        if (sem_timedwait(block, &ts) < 0) {
-            if (errno == ETIMEDOUT) {
-                printf("sync_wait: sem_timewait time out\n");
-            } else {
-                printf("sync_wait: sem_timewait error\n");
+    }
+    else
+    {
+        if (sem_timedwait(block, &ts) < 0)
+        {
+            if (errno == ETIMEDOUT)
+            {
+                printf("sync_wait: sem_timewait time out.\n");
+            }
+            else
+            {
+                printf("sync_wait: sem_timewait error.\n");
             }
             return -1;
         }
@@ -81,18 +95,27 @@ int sync_wait(int nproc)
     return 0;
 }
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if(argc!=3)
+    if (argc != 3)
     {
-        printf("Usage: shellsync <process num> <wait_timeout>\n");
+        printf("Usage: shellsync <process_num> <wait_timeout>.\n");
+        return 1;
     }
     else
     {
-        int proc_num;
-        sscanf(argv[1],"%d",&proc_num);
-        sscanf(argv[2],"%d",&wait_timeout);
-        if(sync_wait(proc_num)<0)
+        int proc_num, wait_timeout;
+        if ((proc_num = atoi(argv[1])) <= 0)
+        {
+            printf("Wrong <process_num>.\n");
+            return 1;
+        }
+        if ((wait_timeout = atoi(argv[2])) <= 0)
+        {
+            printf("Wrong <wait_timeout>.\n");
+            return 1;
+        }
+        if (sync_wait(proc_num, wait_timeout) < 0)
         {
             printf("Processes sync failed.\n");
             sem_unlink(SEM_COUNT_NAME);
@@ -105,4 +128,5 @@ int main(int argc,char *argv[])
             return 0;
         }
     }
+    return 0;
 }
