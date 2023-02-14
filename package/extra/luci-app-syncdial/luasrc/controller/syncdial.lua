1287429ed2e9fc15d193243c98eb1575286a5d2f
HiGarfield <HiGarfield@126.com>
2023-02-15 04:02:36 +0800

    luci-app-syncdial: improve code

--- package/extra/luci-app-syncdial/luasrc/controller/syncdial.lua
+++ package/extra/luci-app-syncdial/luasrc/controller/syncdial.lua
@@ -14,9 +14,22 @@ function index()
 	local page
 	page = entry({"admin", "network", "syncdial"}, cbi("syncdial"), _("Virtual WAN"))
 	page.dependent = true
+	page = entry({"admin", "network", "syncdial", "status"}, call("act_status"))
+	page.leaf = true
 	page = entry({"admin", "network", "macvlan_redial"}, call("redial"), nil)
 	page.leaf = true
 
+end
+
+function act_status()
+	local e = {}
+	local mwan3_status = luci.util.exec("mwan3 status")
+	e.num_online = 0
+	for _ in mwan3_status:gmatch("interface vwan%d+ is online") do
+		e.num_online = e.num_online + 1
+	end
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
 
 function redial()
--- package/extra/luci-app-syncdial/luasrc/model/cbi/syncdial.lua
+++ package/extra/luci-app-syncdial/luasrc/model/cbi/syncdial.lua
@@ -2,34 +2,29 @@
 Sync Dial Luci configuration page.
 Copyright (C) 2015 GuoGuo <gch981213@gmail.com>
 ]] --
-local fs = require "nixio.fs"
-
-local cmd = "mwan3 status | grep -c \"interface vwan[[:digit:]]\\+ is online\""
-local shellpipe = io.popen(cmd, "r")
-local ifnum = shellpipe:read("*a")
-shellpipe:close()
 
 m = Map("syncdial", translate("Add virtual WAN interface"),
-    translate("Add multiple virtual WAN interfaces using macvlan.") .. "<br />" ..
-        translate("Number of online interfaces:") .. " " .. ifnum .. ".")
+    translate("Add multiple virtual WAN interfaces using macvlan."))
 
-s = m:section(TypedSection, "syncdial", translate(" "))
+m:section(SimpleSection).template = "syncdial/syncdial_status"
+
+s = m:section(NamedSection, "config", "syncdial", translate("Config"))
 s.anonymous = true
 
-switch = s:option(Flag, "enabled", translate("Enable"))
-switch.rmempty = false
+enabled = s:option(Flag, "enabled", translate("Enable"))
+enabled.rmempty = false
 
 wannum = s:option(Value, "wannum", translate("Number of virtual WAN interfaces"))
-wannum.datatype = "range(0,20)"
+wannum.datatype = "and(uinteger,range(0,20))"
 wannum.optional = false
 
-diagchk = s:option(Flag, "dialchk", translate("Enable dial check"))
-diagchk.rmempty = false
+dialchk = s:option(Flag, "dialchk", translate("Enable dial check"))
+dialchk.rmempty = false
 
-diagnum = s:option(Value, "dialnum", translate("Minimal number of online interfaces"),
+minonlinenum = s:option(Value, "minonlinenum", translate("Minimal number of online interfaces"),
     translate("Redial if the number of online interfaces is less than this value"))
-diagnum.datatype = "range(0,21)"
-diagnum.optional = false
+minonlinenum.datatype = "and(uinteger,range(0,20))"
+minonlinenum.optional = false
 
 dialwait = s:option(Value, "dialwait", translate("Time waiting before redial"), translate("Minimal value: 5 seconds"))
 dialwait.datatype = "and(uinteger,min(5))"
