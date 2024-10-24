a41ae2fc11499024bb31f98c265ea9f7ca18e9d2
HiGarfield <HiGarfield@126.com>
2024-10-25 00:12:54 +0800

    add -DNDEBUG to TARGET_CFLAGS

--- include/package.mk
+++ include/package.mk
@@ -19,6 +19,8 @@ PKG_IREMAP ?= 1
 
 MAKE_J:=$(if $(MAKE_JOBSERVER),$(MAKE_JOBSERVER) $(if $(filter 3.% 4.0 4.1,$(MAKE_VERSION)),-j))
 
+TARGET_CFLAGS += -DNDEBUG
+
 ifeq ($(strip $(PKG_BUILD_PARALLEL)),0)
 PKG_JOBS?=-j1
 else
