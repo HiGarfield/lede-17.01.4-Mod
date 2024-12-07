63307cd7d9ed527f89aefe2db6b987b9cd476e60
HiGarfield <HiGarfield@126.com>
2024-12-08 02:55:59 +0800

    install_python2.sh: optimize for a quieter build process of Python 2

--- .github/backup/install_python2.sh
+++ .github/backup/install_python2.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+set -e
 
 # Update package list
 sudo apt update
 
 # Install necessary dependencies
-sudo apt install -y build-essential libssl-dev libbz2-dev \
+sudo apt install -y -qq build-essential libssl-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm \
     libxml2-dev libxslt1-dev zlib1g-dev libffi-dev
 
 # Download Python 2.7.x source code (adjust version number as needed)
 PYTHON_VERSION=2.7.18
 rm -rf "Python-$PYTHON_VERSION" "Python-$PYTHON_VERSION.tgz"
-wget "https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz" \
+wget -q "https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz" \
     -O "Python-${PYTHON_VERSION}.tgz"
 
 # Extract the source code
 tar -xzf Python-$PYTHON_VERSION.tgz
-cd Python-$PYTHON_VERSION || exit 1
+cd Python-$PYTHON_VERSION
+
+export CFLAGS="${CFLAGS} -w"
 
 # Configure the build
 ./configure
 
 # Compile and install
-make -j "$(nproc)"
+make -j "$(nproc)" -s
 sudo make altinstall
-sudo ln -s /usr/local/bin/python2.7 /usr/local/bin/python
-sudo ln -s /usr/local/bin/python2.7 /usr/local/bin/python2
+sudo ln -fns /usr/local/bin/python2.7 /usr/local/bin/python
+sudo ln -fns /usr/local/bin/python2.7 /usr/local/bin/python2
 # Clean up
 cd ..
 rm -rf Python-$PYTHON_VERSION Python-$PYTHON_VERSION.tgz
