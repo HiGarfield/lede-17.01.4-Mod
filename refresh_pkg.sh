1707785d691c742bf02d6cd1eb6bc94eba2c3e04
HiGarfield <HiGarfield@126.com>
2025-01-22 22:40:00 +0800

    fix refresh_pkg.sh

--- refresh_pkg.sh
+++ refresh_pkg.sh
@@ -23,6 +23,7 @@ pkg_path="${1%/}"
 }
 
 make "${pkg_path}/download" V=s && {
+    make "${pkg_path}/clean"
     make "${pkg_path}/check" FIXUP=1 V=s
     [ -d "${pkg_path}/patches" ] && 
         make "${pkg_path}/refresh" V=s
